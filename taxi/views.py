afe0f2cb86b59eb0f0dc81ddc3d7cd3997d49b43
JulianskyMe <juliakamynskaya@gmail.com>
2026-04-21 20:37:06 +0300

    Fix

--- taxi/views.py
+++ taxi/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth import get_user_model
 from taxi.forms import DriverLicenseUpdateForm
 from .models import Car, Manufacturer
 from django.contrib.auth import get_user_model
